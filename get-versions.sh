dfcdbd78c50c89247d19860238c73c0d8a661f41
Frederic Leger <frederic@webofmars.com>
2022-11-18 11:12:29 +0100

    feat(ci): limit versions generated

--- get-versions.sh
+++ get-versions.sh
@@ -6,8 +6,10 @@ set -x
 # safer than rm
 echo -n > versions.txt
 
+MAX_VERSIONS=${MAX_VERSIONS:-5}
+
 while read -r VERSION; do
   echo "${VERSION}" >> versions.txt
 done < <(curl -sSq -H "Accept: application/vnd.github+json" https://api.github.com/repos/GoogleContainerTools/skaffold/releases?per_page=100 | \
           jq -r '.[] | select(.prerelease==false) | .tag_name' | \
-          sed 's:^v::' | sort -rV)
+          sed 's:^v::' | sort -rV | head -n ${MAX_VERSIONS})
